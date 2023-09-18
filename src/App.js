b0cde4fe03d99f61aea06f698927c181ba657f5c
LoiTruong31 <loitruong3123@gmail.com>
2023-09-18 13:31:57 +0700

    up header footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import Login from "./Login";
-import TimeSheet from "./components/TimeSheet";
-import './App.css';
+import './App.scss';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./Register";
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 function App() {
   
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="register" element={<Register/>}></Route>
-        <Route path="login" element={<Login />}></Route>
-      </Routes>
-    </BrowserRouter>
+    
+    <div>
+       <Header/>
+      <Home/>
+       <Footer/>
+    </div>
+   
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,34 +3,32 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.scss';
 const Header = (props) => {
-    return(
-        <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
+  return (
+    <Navbar expand="lg" className="test" >
+      <Container >
         <Navbar.Brand href="#home">
-        <div>
-            <img width="85" height="85" src="https://zotek8.com/wp-content/uploads/2023/07/Zotek8_logo_no-slogan_1-1024x1024.png" />
-        </div></Navbar.Brand>
+          <img width="55" height="55" src="https://zotek8.com/wp-content/uploads/2023/07/Zotek8_logo_no-slogan_8.png" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#">TimeSheet</Nav.Link>
+            <Nav.Link href="#link">Device</Nav.Link>
+            <Nav.Link href="#link">Asset</Nav.Link>
+            <Nav.Link href="#link">Assessment</Nav.Link>
+            <Nav.Link href="#link">Staff</Nav.Link>
+            <Nav.Link href="#link">Calendar</Nav.Link>
+            <Nav.Link href="#link">Helpdesk</Nav.Link>
+            <Nav.Link href="#link">MTool</Nav.Link>
+            <Nav.Link href="#link">Feedback</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 }
+
 export default Header;
